9afdc721a162b48fda77798f112c4a318470a847
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-16 13:05:01 +0500

    Move the home page into React

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
@@ -17,12 +18,16 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/no-unknown-property": "error",
+      "react/react-in-jsx-scope": "off",
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
