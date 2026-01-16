6b2eb7b19589bbf2b00c1dc8a720e4f32ecbb5dc
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-16 17:17:02 +0500

    Remove all the anchor tags from our project

--- src/pages/orderspage/OrdersPage.jsx
+++ src/pages/orderspage/OrdersPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import './OrdersPage.css';
 import { Header } from '../../components/Header'
 
@@ -57,11 +58,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
 
                             <div className="product-image-container">
@@ -85,11 +86,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -136,11 +137,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
