69f4ac6c8f2b21a7d5ad37760370b26565080f58
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-19 18:46:27 +0500

    Lift cart up and share it between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { HomePage } from './pages/homepage/HomePage'
 import { Routes, Route } from 'react-router'
 import './App.css'
@@ -5,12 +6,23 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orderspage/OrdersPage';
 import { TrackingPage } from './pages/trackingpage/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+  }, []);
+
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/" element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -4,9 +4,9 @@ import { Header } from '../../components/Header'
 import './HomePage.css'
 
 
-export function HomePage() {
+export function HomePage({ cart }) {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+
 
 
     /*
@@ -36,10 +36,7 @@ export function HomePage() {
                 setProducts(response.data);
             });
 
-        axios.get('/api/cart-items')
-            .then((response) => {
-                setCart(response.data);
-            });
+
     }, []);
 
     return (
