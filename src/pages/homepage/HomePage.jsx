bfe8b9cf563ff329f4d297e44e7ef8b4a84766e3
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-19 17:59:13 +0500

    Change the formating and resolve error of key in HomePage.jsx

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Header } from '../../components/Header'
 import { products } from '../../../starting-code/data/products';
 import './HomePage.css'
-import { data } from 'react-router';
 
 
 export function HomePage() {
@@ -31,56 +30,54 @@ export function HomePage() {
                 <div className="products-grid">
                     {products.map((product) => {
                         return (
-                            <>
-                                <div key={product.key} className="product-container">
-                                    <div className="product-image-container">
-                                        <img className="product-image"
-                                            src={product.image} />
-                                    </div>
-
-                                    <div className="product-name limit-text-to-2-lines">
-                                        {product.name}
-                                    </div>
-
-                                    <div className="product-rating-container">
-                                        <img className="product-rating-stars"
-                                            src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
-                                        <div className="product-rating-count link-primary">
-                                            {product.rating.count}
-                                        </div>
-                                    </div>
-
-                                    <div className="product-price">
-                                        ${(product.priceCents / 100).toFixed(2)}
-                                    </div>
-
-                                    <div className="product-quantity-container">
-                                        <select>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                        </select>
-                                    </div>
-
-                                    <div className="product-spacer"></div>
-
-                                    <div className="added-to-cart">
-                                        <img src="images/icons/checkmark.png" />
-                                        Added
-                                    </div>
-
-                                    <button className="add-to-cart-button button-primary">
-                                        Add to Cart
-                                    </button>
+                            <div key={product.id} className="product-container">
+                                <div className="product-image-container">
+                                    <img className="product-image"
+                                        src={product.image} />
                                 </div>
-                            </>
+
+                                <div className="product-name limit-text-to-2-lines">
+                                    {product.name}
+                                </div>
+
+                                <div className="product-rating-container">
+                                    <img className="product-rating-stars"
+                                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+                                    <div className="product-rating-count link-primary">
+                                        {product.rating.count}
+                                    </div>
+                                </div>
+
+                                <div className="product-price">
+                                    ${(product.priceCents / 100).toFixed(2)}
+                                </div>
+
+                                <div className="product-quantity-container">
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                </div>
+
+                                <div className="product-spacer"></div>
+
+                                <div className="added-to-cart">
+                                    <img src="images/icons/checkmark.png" />
+                                    Added
+                                </div>
+
+                                <button className="add-to-cart-button button-primary">
+                                    Add to Cart
+                                </button>
+                            </div>
                         );
                     })}
                 </div>
