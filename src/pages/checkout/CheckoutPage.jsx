e7301ae764764b750066f1a21c06d9aed63621cb
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-20 18:26:36 +0500

    Separate their own useEffects in checkoutPage

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -12,16 +12,19 @@ export function CheckoutPage({ cart, loadCart }) {
 
     useEffect(() => {
         const fetchCheckoutData = async () => {
-            let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+            const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
             setDeliveryOptions(response.data);
-
-
-            response = await axios.get('/api/payment-summary')
-            setPaymentSummary(response.data);
-        };
+        }
 
         fetchCheckoutData();
+    }, []);
 
+    useEffect(() => {
+        const fetchCheckoutData = async () => {
+            const response = await axios.get('/api/payment-summary')
+            setPaymentSummary(response.data);
+        }
+        fetchCheckoutData();
     }, [cart]);
 
 
