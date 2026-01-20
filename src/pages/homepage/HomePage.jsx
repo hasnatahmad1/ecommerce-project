33548f09a559511844e11fad3799875743c0183b
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-20 17:28:33 +0500

    Create add to Cart Feature

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -5,7 +5,7 @@ import './HomePage.css'
 import { ProductsGrid } from './ProductsGrid';
 
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
     const [products, setProducts] = useState([]);
 
 
@@ -61,7 +61,7 @@ export function HomePage({ cart }) {
             />
 
             <div className="home-page">
-                <ProductsGrid products={products} />
+                <ProductsGrid products={products} loadCart={loadCart} />
             </div>
         </>
     );
