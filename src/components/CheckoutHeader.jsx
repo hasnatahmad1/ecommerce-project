6dfb0f18f5adace76acb064cc92c1d9ba564f4c3
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-20 11:44:59 +0500

    Update Checkout header through backend

--- src/components/CheckoutHeader.jsx
+++ src/components/CheckoutHeader.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router';
 import './CheckoutHeader.css'
 
-export function CheckoutHeader() {
+export function CheckoutHeader({ cart }) {
+    let totalQuantity = 0;
+    cart.map((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
+
     return (
         <>
             <div className="checkout-header">
@@ -15,7 +20,7 @@ export function CheckoutHeader() {
 
                     <div className="checkout-header-middle-section">
                         Checkout (<Link className="return-to-home-link"
-                            to="/">3 items</Link>)
+                            to="/">{totalQuantity} items</Link>)
                     </div>
 
                     <div className="checkout-header-right-section">
