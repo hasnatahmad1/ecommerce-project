6dfc816efff2ca3ba50de06d4c7ffb7c6453eec5
Abul Hasnat Ahmad <abulhasnatahmad12@gmail.com>
2026-01-21 16:29:37 +0500

    Fix the update button

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import { formatMoney } from "../../utils/money";
 import axios from "axios";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+    const [isUpdated, setIsUpdated] = useState(false);
+    const [quantity, setQuantity] = useState(cartItem.quantity);
+
     const deleteCartItem = async () => {
         await axios.delete(`/api/cart-items/${cartItem.productId}`);
         await loadCart();
     };
 
+    const updateQuantity = async () => {
+        await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            quantity: Number(quantity)
+        });
+    };
+
+    const toggleUpdateButton = () => {
+        if (isUpdated) {
+            updateQuantity();
+            loadCart();
+            setIsUpdated(false);
+        } else {
+            setIsUpdated(true);
+        }
+    };
+
+    const inputChange = (event) => {
+        setQuantity(event.target.value);
+    };
 
     return (
         <div className="cart-item-details">
@@ -18,9 +41,21 @@ export function CartItemDetails({ cartItem, loadCart }) {
             </div>
             <div className="product-quantity">
                 <span>
-                    Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+                    Quantity: {isUpdated ? (
+                        <input
+                            type="text"
+                            value={quantity}
+                            onChange={inputChange}
+                            style={{ width: 50 + 'px' }}
+                        />
+                    ) : (
+                        <span className="quantity-label">{cartItem.quantity}</span>
+                    )}
                 </span>
-                <span className="update-quantity-link link-primary">
+                <span
+                    className="update-quantity-link link-primary"
+                    onClick={toggleUpdateButton}
+                >
                     Update
                 </span>
                 <span
